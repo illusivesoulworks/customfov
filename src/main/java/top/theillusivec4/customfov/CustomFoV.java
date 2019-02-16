3615df662cce4319e74dba03615a26b0467c0ef9
TheIllusiveC4 <itwasmec4@gmail.com>
2019-02-15 19:27:32 -0800

    add copyright

--- src/main/java/top/theillusivec4/customfov/CustomFoV.java
+++ src/main/java/top/theillusivec4/customfov/CustomFoV.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018-2019  C4
+ *
+ * This file is part of CustomFoV, a mod made for Minecraft.
+ *
+ * CustomFoV is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CustomFoV is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with CustomFoV.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.theillusivec4.customfov;
 
 import net.minecraftforge.common.MinecraftForge;
